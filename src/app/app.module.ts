49435a753aaf3bd2ac1aece679edf4bfff1f2652
vuongvantu <vuongvantu2001x@gmail.com>
2021-08-19 08:36:03 +0700

    b5-card-name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NameCardComponent } from './name-card/name-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameCardComponent
   ],
   imports: [
     BrowserModule,
